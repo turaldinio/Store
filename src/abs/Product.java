3e10cddee1182d9783d39ef176db9dddb5a38f76
Tural Guluev <tural-98@bk.ru>
2022-07-26 14:02:38 +0300

    initial commit

---
+++ src/abs/Product.java
@@ -0,0 +1,25 @@
+package abs;
+
+public abstract class Product {
+    private String name;
+    private double price;
+    private double rating;
+
+    public Product(String name, double price, double rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
---
+++ src/abs/Store.java
@@ -0,0 +1,17 @@
+package abs;
+
+import java.util.List;
+
+public abstract class Store {
+
+    List<Product> productList;
+
+    public Store(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public String showAllProducts() {
+        return productList.toString().
+                substring(1, productList.toString().length() - 1);
+    }
+}
