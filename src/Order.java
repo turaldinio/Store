f8010767cb287a07758ca3cb7eeaddf3e521490c
Tural Guluev <tural-98@bk.ru>
2022-07-26 14:18:24 +0300

    added Order class

---
+++ src/Order.java
@@ -0,0 +1,19 @@
+import abs.Product;
+
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+public class Order {
+    private List<Product> basket;
+    private double sum;
+
+
+    public void addProductInBasket(Product product) {
+        basket.add(product);
+    }
+
+    public double currentBasketSum() {
+        return basket.stream().flatMapToDouble(x -> DoubleStream.of(x.getPrice())).sum();
+    }
+
+}
